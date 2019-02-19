007dbb60bcbc1e539fe396102f79abfaf8ae57b3
Easyghost195 <jean.yves195@gmail.com>
2019-02-19 15:45:33 +0100

    up

---
+++ options.py
@@ -0,0 +1,13 @@
+from os import environ
+
+Token = environ.get('Token')
+CommandPrefix = '!'
+Description = 'Botty le Bot'
+Owner_Id = '230365801457123328'
+Allow_Id = ['230365801457123328','139869001365979136']
+'''channels'''
+channel_bienvenue = '437158278942949396'
+channel_inscription = '546993174401515541'
+test_bot = '437147715734863872'
+message_alliance = '547006745558646834'
+message_alliance_ig = '547006861161791488'
