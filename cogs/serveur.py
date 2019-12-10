2a32eac8b71ef2fa2029ab6a235dac785af4c73a
FlorianGardes <47028943+FlorianGardes@users.noreply.github.com>
2019-12-10 14:32:23 +0100

    Update serveur.py

--- cogs/serveur.py
+++ cogs/serveur.py
@@ -72,7 +72,7 @@ class serveur:
         await self.bot.send_message(auteur, embed=embedmp)
     
     
-    @bot.command(pass_context = True, hidden = False, brief= "Add role to author")
+    @command.command(pass_context = True, hidden = False, brief= "Add role to author")
     async def addrole(ctx,*args):
         msg = ' '.join(args)
         auteur = ctx.message.author
