f0173d18b9d531c3629d4521ad16576d17bccab0
Easyghost195 <43003134+Easyghost195@users.noreply.github.com>
2020-07-25 00:44:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
 import os
 import sys
-import random
-import asyncio
-import math
-import re
-import requests
-import xlrd
-import extensions
 
 import discord
 from discord.utils import *
 from discord.ext import commands
 
-from datetime import datetime
-from time import gmtime, strftime
-import math
-import datetime
-from discord.ext.commands import Bot
-from discord.ext import commands
-from os import environ
-
 from options_fricen import *
-        
-# Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
+import extensions
+
 bot = commands.Bot(description=Description, command_prefix=CommandPrefix, pm_help = True)
 
 cogs_list = [extensions.Travian,
