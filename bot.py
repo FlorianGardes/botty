ff79fe7acf920ca282007c4810e40696fa3483c8
Easyghost195 <43003134+Easyghost195@users.noreply.github.com>
2020-07-25 15:44:08 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import sys
 import requests
 import datetime
 import discord
+import logging
+import aiohttp
+import asyncpg
 
 from discord.ext import commands
 
@@ -15,21 +18,33 @@ from extensions.serveur import serveur
 
 bot = commands.Bot(description=Description, command_prefix=CommandPrefix, pm_help = True)
 
-bot.load_extension('extensions.travian')
-bot.load_extension('extensions.serveur')
-bot.load_extension('extensions.fun')
+log = logging.getLogger(__name__)
 
-# Permet de vérifier le bon lancement du bot
-@bot.event
-async def on_ready():
-        print('--------------------------------')
-        print('Bot connecté')
-        print('Username : {}'.format(self.bot.user.name))
-        print('ID : {}'.format(self.bot.user.id))
-        print('discord.py v{}'.format(discord.__version__))
-        print('Nombre de serveur infectés:', str(len(self.bot.servers)))
-        print('Nombre de personnes visibles:',len(set(self.bot.get_all_members())))
-        print('--------------------------------')
-        await bot.change_presence(game=(discord.Game(name='{}help'.format(CommandPrefix))))
+initial_extensions = (
+        'extensions.travian',
+        'extensions.fun',
+        'extensions.serveur',
+)
+
+class Botty():
+    def __init__(self):
+        # Permet de vérifier le bon lancement du bot
+        @bot.event
+        async def on_ready():
+                print('--------------------------------')
+                print('Bot connecté')
+                print('Username : {}'.format(self.bot.user.name))
+                print('ID : {}'.format(self.bot.user.id))
+                print('discord.py v{}'.format(discord.__version__))
+                print('Nombre de serveur infectés:', str(len(self.bot.servers)))
+                print('Nombre de personnes visibles:',len(set(self.bot.get_all_members())))
+                print('--------------------------------')
+                for extension in initial_extensions:
+                        try:
+                                self.load_extension(extension)
+                        except Exception as e:
+                                print(f'Failed to load extension {extension}.', file=sys.stderr)
+                                traceback.print_exc()
+                await self.bot.change_presence(game=(discord.Game(name='{}help'.format(CommandPrefix))))
 
 bot.run(Token_Fricen)
