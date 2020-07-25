f7c89f7379162044d453e482238a51d8ef3ea0c8
Easyghost195 <43003134+Easyghost195@users.noreply.github.com>
2020-07-25 15:31:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,22 +3,17 @@ import os
 import sys
 import requests
 import datetime
-
 import discord
-from discord.utils import *
+
 from discord.ext import commands
 
 from options_fricen import *
-import extensions
-from extensions import travian
-from extensions import fun
-from extensions import serveur
 
 bot = commands.Bot(description=Description, command_prefix=CommandPrefix, pm_help = True)
 
-cogs_list = [extensions.Travian,
-             extensions.Serveur,
-             extensions.Fun]
+bot.load_extension('extensions.travian')
+bot.load_extension('extensions.serveur')
+bot.load_extension('extensions.fun')
 
 # Permet de vérifier le bon lancement du bot
 @bot.event
@@ -31,10 +26,6 @@ async def on_ready():
         print('Nombre de serveur infectés:', str(len(self.bot.servers)))
         print('Nombre de personnes visibles:',len(set(self.bot.get_all_members())))
         print('--------------------------------')
-
-        for cog in cogs_list:
-                bot.add_cog(cog(bot))
-
         await bot.change_presence(game=(discord.Game(name='{}help'.format(CommandPrefix))))
 
 bot.run(Token_Fricen)
