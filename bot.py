45f4e6296d81940a419c6a35360501fe53b222a8
Easyghost195 <jean.yves195@gmail.com>
2020-07-25 23:02:43 +0200

    reparation bot.py / fun.py / serveur.py

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 #import os
 #import sys
+#import random
+#import asyncio
+#import math
+#import re
 #import requests
 #import datetime
-import discord
+#import xlrd
 
+import discord
 from discord.ext import commands, tasks
 
 from options_fricen import *
@@ -13,38 +18,23 @@ from options_fricen import *
 #from extensions.fun import fun
 #from extensions.serveur import serveur
 
-#bot = commands.Bot(description=Description, command_prefix=CommandPrefix, pm_help = True)
+bot = commands.Bot(description=Description, command_prefix=CommandPrefix, pm_help = True)
 
-def get_prefix(client, message):
-    prefixes = CommandPrefix
-    return commands.when_mentioned_or(*prefixes)(client, message)
 
-class Botty(commands.Bot):
-        
-        def __init__(self):
-            super().__init__(
-                command_prefix=CommandPrefix,
-                description=Description,
-                pm_help = True,
-            )
-                
-       	    self.load_extension("extensions.fun")
-       	    self.load_extension("extensions.serveur")
-            self.load_extension("extensions.travian")
-        
-        async def on_ready(self):
-            print('--------------------------------')
-            print('Bot connecté')
-            print('Username : {}'.format(self.bot.user.name))
-            print('ID : {}'.format(self.bot.user.id))
-            print('discord.py v{}'.format(discord.__version__))
-            print('Nombre de serveur infectés:', str(len(self.bot.servers)))
-            print('Nombre de personnes visibles:',len(set(self.bot.get_all_members())))
-            print('--------------------------------')
-            await self.bot.change_presence(game=(discord.Game(name='{}help'.format(CommandPrefix))))
-        
-        def run(self):
-            super().run(self.config[Token_Fricen])
-
-bot = Botty()
-bot.run()
+bot.load_extension("extensions.fun")
+bot.load_extension("extensions.serveur")
+bot.load_extension("extensions.travian")
+    
+@bot.event  
+async def on_ready():
+    print('--------------------------------')
+    print('Bot connecté')
+    print('Username : {}'.format(bot.user.name))
+    print('ID : {}'.format(bot.user.id))
+    print('discord.py v{}'.format(discord.__version__))
+    #print('Nombre de serveur infectés:', str(len(bot.servers)))
+    #print('Nombre de personnes visibles:',len(set(bot.get_all_members())))
+    print('--------------------------------')
+    #await bot.change_presence(game=(discord.Game(name='{}help'.format(CommandPrefix))))
+      
+bot.run(Token_Fricen)
--- extensions/serveur.py
+++ extensions/serveur.py
@@ -1,15 +1,11 @@
 import discord
 import asyncio
 import random
+import time
+from datetime import datetime
 from discord.ext import commands
 from options_fricen import *
 from discord.utils import *
-
-def is_allow(ctx):
-    for allowed in Allow_Id:
-        if ctx.message.author.id == allowed:
-            return True
-    return False
 
 class serveur(commands.Cog):
 
@@ -25,18 +21,18 @@ class serveur(commands.Cog):
         await self.bot.say(msg) '''
         
     # Message de bienvenue
-    async def on_member_join(self, member):
+    async def on_member_join(self, ctx, member):
         channel = get(member.server.channels, name='nouveau-venu')
-        await self.bot.send_message(channel,'{0.name} à rejoins le server !'.format(member))
+        await ctx.send_message(channel,'{0.name} à rejoins le server !'.format(member))
         embed=discord.Embed(title="Welcome", color=0x7f07b0)
         embed.add_field(name="G&V server", value = "First, go on #sign-in and type `!sign <pseudo>` with your IG pseudos\nGo in #command for more details on the commands available")
-        await self.bot.send_message(member, embed=embed)
+        await ctx.send_message(member, embed=embed)
         #await self.bot.send_message(member,'Bienvenue sur le serveur !\n!help pour avoir les commandes disponibles'.format(member))
 
     @commands.command(pass_context=True, aliases=['servinfo', 'infoserv', 'infoserver'])
     async def serverinfo(self, ctx):
         """Give some information about this server discord"""
-        server = ctx.message.server
+        server = ctx.message.guild
         online = len([m.status for m in server.members
                         if m.status == discord.Status.online or
                         m.status == discord.Status.idle])
@@ -44,7 +40,7 @@ class serveur(commands.Cog):
         salons_textuels = len([x for x in server.channels
                                 if x.type == discord.ChannelType.text])
         salons_vocaux = len(server.channels) - salons_textuels
-        jours = (ctx.message.timestamp - server.created_at).days
+        jours = (ctx.message.created_at - server.created_at).days
         creation = ("Since {}. That's over {} days ago !""".format(server.created_at.strftime("%d %b %Y"), jours))
 
         data = discord.Embed(description=creation, colour=discord.Colour(value=0x206694))
@@ -54,7 +50,7 @@ class serveur(commands.Cog):
         data.add_field(name="Voice Channels", value=salons_vocaux)
         data.add_field(name="Roles", value=len(server.roles))
         data.add_field(name="Owner", value=str(server.owner))
-        data.set_footer(text="Server ID: " + server.id)
+        data.set_footer(text="Server ID: " + str(server.id))
 
         if server.icon_url:
             data.set_author(name=server.name, url=server.icon_url)
@@ -62,50 +58,36 @@ class serveur(commands.Cog):
         else:
             data.set_author(name=server.name)
 
-        await self.bot.say(embed=data)
+        await ctx.send(embed=data)
 
     @commands.command(pass_context=True, aliases=['inv'], hidden = True)
     async def invite(self, ctx):
         """Sent an invitation in pm of the server"""
         channel = discord.Object(id=channel_bienvenue)
         auteur = ctx.message.author
-        server = ctx.message.server
-        link = await self.bot.create_invite(destination = channel, temporary = False, max_uses = 0)
+        server = ctx.message.guild
+        link = await ctx.channel.create_invite(temporary = False, max_uses = 0)
         embedmp = discord.Embed(color=0xf41af4)
         embedmp.add_field(name="Discord invitation link:", value=link)
         embedmp.set_footer(text="%s invited link"%server)
         embed = discord.Embed(description ="Invitation sent in Private Message to **%s**"%auteur, color=0xf41af4)
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, embed=embed)
-        await self.bot.send_message(auteur, embed=embedmp)
-
-
-    @commands.command(pass_context = True, hidden = True, brief= "Add role to author")
-    async def addrole(ctx,*args):
-        msg = ' '.join(args)
-        auteur = ctx.message.author
-        role_serveur = [roles.name.lower() for roles in ctx.message.server.roles]
-        role_member = [roles.name.lower() for roles in ctx.message.author.roles]
-        if (msg not in role_serveur):
-            if(msg not in role_member):
-                role = await self.bot.create_role(auteur.server, name=msg)
-                await selfbot.say('%s has been added by %s'%(msg,auteur) )
-            await self.bot.say("You have already this role")
-        await self.bot.say("Role has not been created")
+        await ctx.message.delete()
+        await ctx.send(ctx.message.channel, embed=embed)
+        await ctx.author.send(auteur, embed=embedmp)
 
 
     @commands.command(pass_context = True, hidden=True)
     async def createrole(self, ctx, *args):
         """Créé un role"""
-        role = [roles.name.lower() for roles in ctx.message.server.roles]
+        role = [roles.name.lower() for roles in ctx.message.guild.roles]
         if 'admin' not in role:
-            return await self.bot.say("**Sorry you are not allowed to make this order!**")
+            return await ctx.send("**Sorry you are not allowed to make this order!**")
         msg = ' '.join(args)
         auteur = ctx.message.author
         color = ''.join([random.choice('0123456789ABCDEF') for x in range(6)])
         color = int(color, 16)
-        role = await self.bot.create_role(auteur.server, name=msg, colour=discord.Colour(color))
-        await self.bot.say('Role créé avec succes par %s'%auteur )
+        role = await ctx.guild.create_role(name=msg, colour=discord.Colour(color))
+        await ctx.send('Role créé avec succes par %s'%auteur )
 
 
 
@@ -115,25 +97,25 @@ class serveur(commands.Cog):
         role = [roles.name.lower() for roles in ctx.message.author.roles]
 
         if 'admin' not in role:
-            return await self.bot.say("**Sorry you are not allowed to make this order!**")
+            return await ctx.send("**Sorry you are not allowed to make this order!**")
         if not member:
-            return await self.bot.say(ctx.message.author.mention + ", please specify the member to kick")
+            return await ctx.send(ctx.message.author.mention + ", please specify the member to kick")
         embed = discord.Embed(description = "**%s** à été kick"%member.name, color = 0xF00000)
         embed.set_footer(text="Bye bye")
-        await self.bot.kick(member)
-        await self.bot.say(embed = embed)
+        await ctx.guild.kick(member)
+        await ctx.send(embed = embed)
 
     @commands.command(pass_context = True, hidden=True)
-    @commands.check(is_allow)
+    @commands.has_permissions(administrator=True)
     async def clear(self, ctx, lignes):
-        mgs = []
+        msg = []
         lignes = int(lignes)
-        async for x in self.bot.logs_from(ctx.message.channel, limit = lignes+1):
-            mgs.append(x)
-        await self.bot.delete_messages(mgs)
+        async for x in ctx.channel.history(limit = lignes):
+            msg.append(x)
+        deleted = await ctx.message.channel.delete_messages(msg)
         embed = discord.Embed(description = "**%s** message deleted by **%s**"%(lignes, ctx.message.author), color = 0xF00000)
         embed.set_footer(text="Clear")
-        await self.bot.say(embed = embed)
+        await ctx.send(embed = embed)
 
 def setup(bot):
         bot.add_cog(serveur(bot))
