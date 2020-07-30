b3ed228e75f1507222e0d9fad47e93baef7deca9
Easyghost195 <jean.yves195@gmail.com>
2020-07-30 21:13:32 +0200

    modif bot.py

--- bot.py
+++ bot.py
@@ -15,8 +15,6 @@ from discord.ext import commands, tasks
 from options_fricen import *
 
 bot = commands.Bot(description=Description, command_prefix=CommandPrefix, pm_help = True)
-
-
 bot.load_extension("extensions.fun")
 bot.load_extension("extensions.serveur")
 bot.load_extension("extensions.travian")
@@ -29,18 +27,21 @@ async def on_ready():
     print('ID : {}'.format(bot.user.id))
     print('discord.py v{}'.format(discord.__version__))
     print('--------------------------------')
-"""     
+
 @bot.event
 async def on_message(message):
+    if message.channel.id != channel_inscription:
+        await bot.process_commands(message)
     if message.author == bot.user: #immunité au bot
         return
-    if message.channel.id != channel_inscription:
-        return
-    message_content = message.content
-    sign_message = message_content[:5]
-    channel = message.channel
-    if sign_message != '$sign':
-        await message.delete()
-        await channel.send("**%s**"%(str(message.author)) +", use the `$sign` command please!")   
-"""   
+    if message.channel.id == channel_inscription:
+        message_content = message.content
+        sign_message = message_content[:5]
+        channel = message.channel
+        if sign_message != '$sign':
+            await message.delete()
+            await channel.send("**%s**"%(str(message.author)) +", use the `$sign` command please!")
+        else:
+            await bot.process_commands(message)
+
 bot.run(Token_Fricen)
