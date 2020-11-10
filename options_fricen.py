1fcdb50aceb9f967f47bc3f1d38009650f660e0a
Florian <fl.gardes@yahoo.fr>
2020-11-10 12:39:22 +0100

    ajout des liens hypertexte

--- options_fricen.py
+++ options_fricen.py
@@ -8,8 +8,14 @@ Allow_Id = [230365801457123328, 139869001365979136, 142596227207397377]
 #             Easyghost#6478        Fricen#2441         kwtis#3365
 '''channels'''
 channel_bienvenue = 547111805395140625
-channel_inscription = 373896386866970625
+channel_inscription = 773988388863868988
 hc_using_bot = 541923416832475136
 message_alliance = 373549029575098369#info-hc
 message_def = 547109529045696522#info-def
 message_alliance_ig = 546765589818114108
+
+'''lien IG'''
+serveur_travian = "https://final.travian.com"
+getter = "https://www.gettertools.com/final.travian.com/"
+kiri = "http://travian.kirilloid.ru/"
+gdoc_def = "https://docs.google.com/spreadsheets/d/1DEgTNDbJmdO4rV5HYM2hDAShiCYlcZk_8C-VaLLGEJg/edit?ts=5ee0b9b5#gid=440791252"
