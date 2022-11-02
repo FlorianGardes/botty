2770fe541e32683b9833df46e2b08556efaf001f
FlorianGardes <47028943+FlorianGardes@users.noreply.github.com>
2022-11-02 22:55:20 +0100

    Update options_fricen.py
    ds

--- options_fricen.py
+++ options_fricen.py
@@ -8,7 +8,7 @@ Allow_Id = [230365801457123328, 139869001365979136, 142596227207397377]
 #             Easyghost#6478        Fricen#2441         kwtis#3365
 '''writing channels'''
 channel_bienvenue = 547111805395140625
-channel_inscription = 987971217061449758
+channel_inscription = 1037479253391978557
 hc_using_bot = 541923416832475136
 message_alliance = 373549029575098369#info-hc
 message_def = 547109529045696522#info-def
@@ -19,10 +19,10 @@ spam_room = 373586644873510912
 radio = 752174689366835421
 
 '''lien IG'''
-serveur_travian = "https://ttq.x2.asia.travian.com"
-getter = "https://www.gettertools.com/ttq.x2.asia.travian.com.2"
+serveur_travian = "https://finals.travian.com"
+getter = "https:/www.gettertools.com/finals.travian.com"
 kiri = "http://travian.kirilloid.ru/"
-gdoc_def = "https://docs.google.com/spreadsheets/d/1c6PjWZQJpDx32R8pkpZSVwT-MST1aGwd-oKFTJeBn3k/edit#gid=440791252"
+gdoc_def = "https://docs.google.com/spreadsheets/d/1dPkL7cQ_qEbK0cQtg3nXNtViboLu80kyfAzOl--DK8s/edit#gid=440791252"
 site_gv = "https://gvteam.net/"
 radio_link = "https://puccaradio.com/"
 allies = "A-秦王,A-神教,A-長城,A-爱你,A-汉武,A-故宫,A-天启,A-H™,A-OGC,A-CAT,SPQR™,SPQR,SPQER,A-RB,bAnd,A-Bo,COT™,BEST™,#BEST,BEST©,BestT,A-BND,NEW"
